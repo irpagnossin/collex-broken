c968c9271a606af2bc8dbf6a456c0c10e2f6c228
Ivan Ramos Pagnossin <ivan.pagnossin@gmail.com>
2018-12-18 22:08:55 -0200

    Add many physics questions

---
+++ all_questions.py
@@ -0,0 +1,22 @@
+import jinja2
+import os
+from glob import glob
+from jinja2 import Template
+latex_jinja_env = jinja2.Environment(
+	block_start_string = '\BLOCK{',
+	block_end_string = '}',
+	variable_start_string = '\VAR{',
+	variable_end_string = '}',
+	comment_start_string = '\#{',
+	comment_end_string = '}',
+	line_statement_prefix = '%%',
+	line_comment_prefix = '%#',
+	trim_blocks = True,
+	autoescape = False,
+	loader = jinja2.FileSystemLoader(os.path.abspath('.'))
+)
+
+template = latex_jinja_env.get_template('all_questions.jinja.tex')
+
+with open("all_questions.tex", "w") as file:
+    file.write(template.render(glob=glob))
